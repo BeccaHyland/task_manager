f8a6320904df66afe32f073ef19b2a17e9eafffe
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-10 20:13:07 -0600

    DRY refactor for Task class

--- app/models/task.rb
+++ app/models/task.rb
@@ -18,8 +18,7 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    Task.database
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -27,9 +26,14 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    Task.database
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
   end
 end
